211fdc6a0dadacf39853a2480e87ee71d72e0322
KomaR1 <31096313+KomaR1@users.noreply.github.com>
2018-06-01 19:50:18 +0300

    Add files via upload

--- year.py
+++ year.py
@@ -1,18 +1,18 @@
-
+"""This program define the season of year by number of month"""
 def season(num_of_month):
     if num_of_month == 12:
         print("It's winter!")
     elif num_of_month <= 2:
-        print("It's winter!")
+        print("It's Winter!")
     elif num_of_month >= 3:
         if num_of_month <= 5:
-            print("It's spring!")
+            print("It's Spring!")
     elif num_of_month >= 6:
         if num_of_month <= 8:
-            print("It's summer!")
+            print("It's Summer!")
     elif num_of_month >= 9:
         if num_of_month <= 11:
-            print("It's autumn!")
+            print("It's Autumn!")
 print("Enter the number of month: ")
 a = int(input())
 season(a)
