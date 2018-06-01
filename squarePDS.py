b85982cf43febf450f297d9f871cf2e2d29124cc
KomaR1 <31096313+KomaR1@users.noreply.github.com>
2018-06-01 19:41:54 +0300

    Add files via upload

--- squarePDS.py
+++ squarePDS.py
@@ -1,8 +1,11 @@
+"""This program is calculating perimeter, area and diagonal of a square"""
 import math
 def square(side):
     P = side * 4
-    S = side * side
+    A = side * side
     D = math.sqrt(2) * side
     t = (P,S,D)
     print(t)
+print("Enter the side of square")
+side = float(input())
 square(5)
